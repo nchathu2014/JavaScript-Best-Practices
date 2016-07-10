e9e66bc815ba05372b6689810e18ea95c4a9fb64
Chathuranga <UCHATNU@PEROOT.COM>
2016-07-10 22:35:06 +0530

    finish the syntaxt section

---
+++ src/Syntax/5 - variables/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by UCHATNU on 7/10/2016.
+ */
+
+
+
+var test=10;
+function func(){
+    test=89;
+    var test;
+}
+func();
+
+console.log(test);
---
+++ src/Syntax/6 - functions/script.js
@@ -0,0 +1,32 @@
+/**
+ * Created by UCHATNU on 7/10/2016.
+ */
+
+
+
+
+function declarationFunc(){
+    console.log("I am a declaration functions");
+}
+
+var expressionFunc = function(){
+    console.log("I am a expressive functions");
+}
+
+expressionFunc();
+declarationFunc();
+
+//best practice is that 1-declare the variables and second declare the functions and then do the code run.
+
+/*
+*
+* var x = 10;
+
+ function getX(){
+ console.log(x);
+ }
+
+ console.log(getX());
+
+ *
+* */
